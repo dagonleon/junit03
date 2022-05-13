4c7a76d2abe900b40fc2c0de952bfdb0212287cb
DavidGonzalez <davidgonzaleon18@gmail.com>
2022-05-13 08:54:43 +0200

    cambios FacturaTest

--- src/test/java/FacturaTest.java
+++ src/test/java/FacturaTest.java
@@ -9,6 +9,8 @@ class FacturaTest {
         Factura f = new Factura();
         Producto p1 =  new Producto("pan",0.50f, 2);
         Producto p2 = new Producto("jamon",3.50f, 4);
+        f.meterProducto(p1);
+        f.meterProducto(p2);
         assertEquals(8f, f.totalFatura(), 0.1);
     }
 
@@ -17,6 +19,8 @@ class FacturaTest {
         Factura f = new Factura();
         Producto p1 =  new Producto("pan",0.50f, 2);
         Producto p2 = new Producto("jamon",3.50f, 4);
+        f.meterProducto(p1);
+        f.meterProducto(p2);
         assertEquals(9.68f, f.aplicarIva(21), 0.1);
     }
 }
